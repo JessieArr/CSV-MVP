24bb5dd719e0e12f27f8d3d4564545323e75a7a7
ShaWn <smorrison1986@gmail.com>
2020-05-26 20:31:30 -0500

    Initializing Avalonia application.

---
+++ CSVMVP/ViewModels/ViewModelBase.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ReactiveUI;
+
+namespace CSVMVP.ViewModels
+{
+    public class ViewModelBase : ReactiveObject
+    {
+    }
+}
